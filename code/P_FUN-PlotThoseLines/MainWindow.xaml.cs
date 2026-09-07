526ed3f5aa672409aca02bcd55d65398d4c52b21
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-09-07 10:59:28 +0200

    feat(front): Ajout d'un frontend de base pour l'application
    [120][DONE]
    
    Remarque : il a fallu se réapproprié le fonctionnement du xaml pour le front, des problèmes d'alignement sont survenu et il a fallu se documenter sur certaines choses (définition des colones/ligne, placeholder dans les textBox, comment ajouter et utiliser un dockpanel avec un menu

--- code/P_FUN-PlotThoseLines/MainWindow.xaml.cs
+++ code/P_FUN-PlotThoseLines/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace P_FUN_PlotThoseLines
-{
+namespace P_FUN_PlotThoseLines {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+
+
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+            WpfPlot1.Refresh();
         }
+
     }
+}
+
+// Source - https://stackoverflow.com/a/12593753
+// Posted by Brian Hinchey, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-09-07, License - CC BY-SA 3.0
+
+public static class Constants {
+    public const string DateTimeUiFormat = "dd/MM/yyyy";
+
+    //etc...
 }
