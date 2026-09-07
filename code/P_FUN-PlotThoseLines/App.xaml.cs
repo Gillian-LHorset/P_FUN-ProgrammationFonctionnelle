f493aeab08101ba808bb64e06a4b002e90092445
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-09-07 08:53:14 +0200

    feat(code): Ajout du projet avec le framework WPF
    [10][DONE]

---
+++ code/P_FUN-PlotThoseLines/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P_FUN_PlotThoseLines
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
