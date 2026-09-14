507dcf9c1cd28762d8427927523409460c92c9bc
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-09-14 08:59:44 +0200

    feat(dataImport): Ajout du parse des données d'un csv vers un type TemperatureSet
    [35][DONE]

--- code/P_FUN-PlotThoseLines/MainWindow.xaml.cs
+++ code/P_FUN-PlotThoseLines/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using DataSeries;
+using Microsoft.Win32;
+using System.Windows;
 
 namespace P_FUN_PlotThoseLines {
     /// <summary>
@@ -15,15 +17,28 @@ namespace P_FUN_PlotThoseLines {
             WpfPlot1.Refresh();
         }
 
+
+        public void dataImport(object sender, RoutedEventArgs e) {
+            var openFile = new OpenFileDialog();
+            openFile.DefaultExt = "*.csv";
+            openFile.Filter = "Csv Files (*.csv)|*.csv";
+            if (openFile.ShowDialog() == true) {
+                string filePath = openFile.FileName;
+
+                DataSeries<TemperatureSet> temperatures;
+                temperatures = DataSeries<TemperatureSet>.FromCsv(filePath, ParseTemperatrue);
+
+                MessageBox.Show($"count : {temperatures.Count}");
+            }
+        }
+
+        TemperatureSet ParseTemperatrue(string[] cols) {
+            TemperatureSet temp = new TemperatureSet(cols[0], DateTime.Parse(cols[1]), Double.Parse(cols[2]));
+
+            return temp;
+        }
+
+
     }
 }
 
-// Source - https://stackoverflow.com/a/12593753
-// Posted by Brian Hinchey, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-09-07, License - CC BY-SA 3.0
-
-public static class Constants {
-    public const string DateTimeUiFormat = "dd/MM/yyyy";
-
-    //etc...
-}
